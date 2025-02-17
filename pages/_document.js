52fa40735e2bcf2980bc66d68c5f3b8b49ee4d2a
black-onyx <75280597+black-onyx@users.noreply.github.com>
2025-02-17 13:36:35 +0000

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,23 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="theme-compiled">
+        <Head>
+          <!-- Matomo -->
+              <script>
+                var _paq = window._paq = window._paq || [];
+                /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+                _paq.push(['trackPageView']);
+                _paq.push(['enableLinkTracking']);
+                (function() {
+                  var u="//matomo.kudobuzz.com/matomo/";
+                  _paq.push(['setTrackerUrl', u+'matomo.php']);
+                  _paq.push(['setSiteId', '1']);
+                  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                  g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+                })();
+              </script>
+              <!-- End Matomo Code -->
+
         <Head />
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
